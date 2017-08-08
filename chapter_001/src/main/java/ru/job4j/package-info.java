be3d5bf8c837c4be298311199b7da120640b63ac
Andrey Sagirov <andrey.sagirov23gmail.com>
2017-08-08 13:28:23 +0300

    task 4087

---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,22 @@
+/**
+* Package for calculate task.
+*
+* @author Andrey Sagirov (mailto:andrey.sagirov23@gmail.com)
+* @version $Id$
+* @since 08.08.2017
+*/
+package ru.job4j;
+/**
+ *Class Calculate решение задачи части 001 урок 1.
+ *@author asagirov
+ *@since 08.08.2017
+*/
+
+public class Calculate {
+	/**
+	* Метод для вывода текста в консоли
+	*/
+	public static void main(String[] args) {
+	System.out.println("Hello World");
+	}
+}
