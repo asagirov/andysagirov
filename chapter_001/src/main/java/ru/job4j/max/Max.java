5613a6125df27751dc3e150ad9837fbe4ce8d481
Andrey Sagirov <andrey.sagirov23gmail.com>
2017-08-18 13:33:14 +0300

    3.4. Максимум из трех чисел task 190

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -14,4 +14,6 @@ public class Max {
     public int max(int first, int second) {
        return first > second ? first : second;
     }
+	public int max(int first, int second, int third) {
+       return Math.max (first, second, third);
 }
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -18,16 +18,26 @@ public class MaxTest {
 	* @Test для проверки.
 	*/
 	@Test
-public void whenFirstLessSecond() {
+	/**
+	* Проверка когда 2-е число макс.
+	*/
+public void whenFirstIsMaximum() {
     Max maximum = new Max();
-    int result = maximum.max(4, 15);
+    int result = maximum.max(4, 15, 10);
     assertThat(result, is(15));
 }
 /**
-	* Проверка когда 1-ое число больше 2-го.
+	* Проверка когда 1-е число макс.
 	*/
-public void whenSecondLessFirst() {
-    int result = maximum.max(12, 0);
+public void whenFirstIsMaximum() {
+    int result = maximum.max(12, 0, 11);
     assertThat(result, is(12));
+}
+/**
+	* Проверка когда 3-е число макс.
+	*/
+public void whenThirdIsMaximum() {
+    int result = maximum.max(29, -44, 34);
+    assertThat(result, is(34));
 }
 }
